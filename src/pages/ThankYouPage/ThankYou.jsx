7b6c5829fc43f6722f1bb01d603d36965f14574d
nnataali <natalia.charkviani.1@btu.edu.ge>
2022-12-27 19:25:40 +0400

    style: add thank you page animation

--- src/pages/ThankYouPage/ThankYou.jsx
+++ src/pages/ThankYouPage/ThankYou.jsx
@@ -1,9 +1,24 @@
 import ThankYouImage from '../../assets/images/thank-you-image.png';
-// import '../../App';
+import Star from '../../assets/images/star.png';
+import './ThankYou.css';
 const ThankYou = () => {
   return (
-    <div className='h-screen bg-dark-gray flex justify-center items-center '>
-      <img src={ThankYouImage} alt='Thank you' className='hidden' />
+    <div className='h-screen bg-dark-gray flex flex-col justify-center items-center div '>
+      <img
+        src={Star}
+        alt='Big star'
+        className=' w-12 big-star-anim  absolute invisible'
+      />
+      <img
+        src={ThankYouImage}
+        alt='Thank you'
+        className=' visibility-animation thanks-image absolute invisible'
+      />
+      <img
+        src={Star}
+        alt='Tiny star'
+        className=' w-8 tiny-star-anim  absolute invisible'
+      />
     </div>
   );
 };
